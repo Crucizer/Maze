45239c934584a4a8eac88216e97906e442250e14
Ayush Agarwal <ayushdiagarwal@gmail.com>
2024-06-29 13:43:10 +0530

    first commit

---
+++ maze.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+int main () {
+
+    // using a vector
+    int size = 4;
+
+    std::vector<std::vector<int> > maze(size, std::vector<int>(size, 0));
+
+
+
+    std::cout << "Hello cunts" << '\n';
+    return 0;
+}
